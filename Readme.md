fdd0acdeb4fb79fee2da85e9a7a05870eac57446
Jim Bulkowski <jim.b@paperelectron.com>
2016-03-04 10:35:39 -0500

    Initial

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Pomegranate Express Preroute Middleware
+
+Mounts middleware and express configuration before routes are loaded.
+Used in [Pomegranate Express](https://github.com/Pomegranate/pomegranate-express).
+Provided here for standalone use.
---
+++ index.js
@@ -0,0 +1,74 @@
+/**
+ * @file index
+ * @author Jim Bulkowski <jim.b@paperelectron.com>
+ * @project pomegranate-express-preroute-middleware
+ * @license MIT {@link http://opensource.org/licenses/MIT}
+ */
+
+"use strict";
+var _ = require('lodash');
+
+/**
+ * Configures the Express application and mounts all pre-route middleware.
+ * @module PreMiddleware
+ * @injector {None} Adds nothing to the injector.
+ * @property {Object} options Plugin Options
+ * @property {Array} options.order Mount order of middleware functions.
+ *
+ */
+
+module.exports = {
+  options: {
+    middlewareOrder: ['compress', 'serveStatic','responseTime', 'logger']
+  },
+  metadata: {
+    name: 'PreRouter',
+    layer: 'pre_router',
+    type: 'none'
+  },
+  plugin: {
+    load: function(inject, loaded){
+      var self = this;
+      inject(function(Express, Middleware, ExpressConfig){
+
+        // Bring in all of the Express configurations.
+        self.Logger.log('Configuring Express.')
+        var a = _.chain(ExpressConfig)
+          .map(function(fn, prop){
+            return {name: prop, fn: fn}
+          })
+          .filter(function(obj) {
+            return _.isFunction(obj.fn)
+          })
+          .each(function(confFn){
+            self.Logger.log('Setting config for ' + confFn.name)
+            confFn.fn(Express)
+          })
+          .value()
+
+        // Bring in all of the merged middlewares.
+        self.Logger.log('Adding Pre-Router middleware.')
+        _.chain(self.options.middlewareOrder)
+          .map(function(p){
+            return {fn: Middleware[p], name: p}
+          })
+          .filter(function(mw){
+            return _.isFunction(mw.fn);
+          })
+          .each(function(vmw){
+            self.Logger.log(vmw.name + ' Middleware added before routes.');
+            Express.use(vmw.fn)
+          })
+          .value()
+      });
+
+      loaded(null, null)
+    },
+    start: function(done){
+      done()
+    },
+    stop: function(done){
+      done()
+    }
+  }
+}
